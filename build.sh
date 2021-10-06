6078017d316ae0da2eade088d2875d65b813afbb
David Webb <dave@davidwebb.me.uk>
2021-10-06 10:07:34 +0100

    Gold plating

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SOURCE=`pwd`
+# Set temporary working directory
 OUTPUT=/var/tmp
-ELECTRON_VERSION=`curl --silent "https://api.github.com/repos/electron/electron/releases/latest" | jq --raw-output .tag_name | cut -c2-`
 
+# function to run nativefier and copy .app file
 function create_app {
     NAME=$1
     ICON=$2
@@ -11,7 +11,9 @@ function create_app {
     shift 3
     EXTRAS=$*
 
-    nativefier --electron-version ${ELECTRON_VERSION} ${EXTRAS} --name "${NAME}" --icon "${ICON}" --fast-quit  "${URL}" "${OUTPUT}"
+    echo Building ${NAME}...
+
+    nativefier --electron-version ${ELECTRON_VERSION} ${EXTRAS} --name "${NAME}" --icon "${ICON}" --fast-quit  "${URL}" "${OUTPUT}" >/dev/null  2>&1
     
     pkill "${NAME}"
 
@@ -24,15 +26,29 @@ function create_app {
     popd
 }
 
+# Make pushd and popd silent
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+# Make sure we're in the directory with the source files
+cd `dirname ${BASH_SOURCE}`
+
+# Clear out working files in case previous run was interupted
+rm -rf ${OUTPUT}/darwin-x64-template
+
+# Update Nativefier
 brew upgrade nativefier
 
-for SCRIPT in `ls *.js`
-do
-    cp ${SCRIPT} ${OUTPUT}
-done
+# Use most recent version of electron
+ELECTRON_VERSION=`curl --silent "https://api.github.com/repos/electron/electron/releases/latest" | jq --raw-output .tag_name | cut -c2-`
+echo Using Election version ${ELECTRON_VERSION}
 
 create_app "Board Game Arena" bga.png https://boardgamearena.com/
 create_app "UniFi Network" ubiquiti.png https://furia.home:8443/ --ignore-certificate
 create_app "BBC Sounds" bbc-sounds.png https://www.bbc.co.uk/sounds --internal-urls 'bbc.com' --internal-urls 'bbc.co.uk'
 create_app "Overcast" overcast.png https://overcast.fm/podcasts --inject './overcast.js'
-
